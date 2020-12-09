408bf0280453788f798604bd6a91200b732846ee
Ryan Richetto <rrichetto15@gmail.com>
2020-12-08 17:14:33 -0800

    add classes

--- hangman/app.js
+++ hangman/app.js
@@ -1,15 +1,14 @@
-
 const puzzleEl = document.querySelector('#puzzle');
 const guessesEl = document.querySelector('#guesses');
-const game1 = new Hangman('Cat', 2);
+const game1 = new Hangman('Car Parts', 2);
 
-puzzleEl.textContent = game1.getPuzzle();
-guessesEl.textContent = game1.getStatusMessage();
+puzzleEl.textContent = game1.puzzle;
+guessesEl.textContent = game1.statusMessage;
 console.log(game1.status);
 
 window.addEventListener('keypress', function (e) {
   const guess = String.fromCharCode(e.charCode)
   game1.makeGuess(guess);
-  puzzleEl.textContent = game1.getPuzzle();
-  guessesEl.textContent = game1.getStatusMessage();
+  puzzleEl.textContent = game1.puzzle;
+  guessesEl.textContent = game1.statusMessage;
 })
